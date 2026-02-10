4424589fc968c99136b7d6ddd0dc77aa4e615063
Ahnon <arnon.miang@gmail.com>
2026-02-10 11:08:15 +0000

    First Reset with Model

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+use serialport::{StopBits, Parity, DataBits};
+use std::io::Write;
+use std::time::Duration;
+use std::thread;
+
 fn main() {
-    println!("Hello, world!");
+    let mut port = loop {
+        match serialport::new("COM1", 9600)
+            .data_bits(DataBits::Eight)
+            .parity(Parity::Even)
+            .stop_bits(StopBits::One)
+            .timeout(Duration::from_secs(1))
+            .open()
+        {
+            Ok(p) => {
+                println!("Connected to COM1");
+                thread::sleep(Duration::from_secs(10));
+                break p;
+            }
+            Err(e) => {
+                println!("Error: {}. Retry in 2s...", e);
+                thread::sleep(Duration::from_secs(10));
+            }
+        }
+    };
+
+    let frame: [u8; 5] = [0xFC, 0x05, 0x40, 0x2B, 0x15];
+    port.write_all(&frame).unwrap();
+
+    println!("Sent ID003 frame");
 }
